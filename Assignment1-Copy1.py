b3deadc3f48389e65483c33f8959c635bc7e7489
faraz4trance <74188958+faraz4trance@users.noreply.github.com>
2020-11-09 13:41:20 +0100

    Assignment submitted

---
+++ Assignment1-Copy1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+# Q2
+print('lets upgrade'.upper())
+
+
+# In[ ]:
+
+
+#Q3
+Cost = input()
+Sell = input()
+if Sell > Cost:
+    print('Profit')
+elif Sell < Cost:
+    print('Loss')
+else: 
+    print('Neither')
+
+
+# In[25]:
+
+
+#Q4
+x = int(input())
+y=x*80
+print(y)
+
+
+# In[36]:
+
+
+#Q1 (I did not understand the question)
+x='OBANWRI'
+
+
+# In[ ]:
+
+
+
+
